7df10d058a5de163dd006f9951259ed689aeefa4
ben <degarcia.ben@gmail.com>
2021-02-13 12:00:03 -0600

    front end post routes

--- public/js/view.js
+++ public/js/view.js
@@ -1,5 +1,23 @@
-document.addEventListener('DOMContentLoaded', (e) => {
-  console.log('DOM loaded!');
+document.addEventListener("DOMContentLoaded", (e) => {
+  console.log("DOM loaded!");
 
-  // your code here...
+  const form = document.getElementById("todo-form");
+  const newTodoInput = document.querySelector("input.new-item");
+
+  form.addEventListener("submit", e => {
+    e.preventDefault()
+    const text = newTodoInput.value
+
+    fetch('api/todos', {
+      method:'POST',
+      body: JSON.stringify({ text }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .then(err => console.err(err))
+
+  })
 });
